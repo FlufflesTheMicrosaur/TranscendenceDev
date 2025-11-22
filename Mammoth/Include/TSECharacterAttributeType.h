2774d0b1cd14682350bc60cd5067229e18f03f72
FlufflesTheMicrosaur <Fluffles.The.Microsaur@gmail.com>
2025-11-22 15:10:12 -0800

    feat: 101273: begin adding implementation

--- Mammoth/Include/TSECharacterAttributeType.h
+++ Mammoth/Include/TSECharacterAttributeType.h
@@ -10,12 +10,24 @@
 class CCharacterAttributeType : public CDesignType
 	{
 	public:
+		enum ECharacterAttributeCategory {
+			//	Well-defined engine categories
+
+			eCharacterGenome =			0,	//Species of the character (e.g.: Augmented Human, Microsaur, AI, Erebian, etc)
+			eCharacterGender =			1,	//Gender of the character (e.g.: Feminine, Masculine, Plural, Other, None, etc)
+			eCharacterClass =			2,	//Character class of the character (e.g.: Pilgrim, Mercenary, Trader, Explorer, etc)
+
+			//	Custom categories
+
+			eCharacterCustomAttrib =	-1,	//An XML-defined category
+			eCharacterStackableAttrib =	-2,	//An XML-defined category that can be stacked
+			};
+
 		//	CDesignType overrides
 
 		static CCharacterAttributeType *AsType (CDesignType *pType) { return ((pType && pType->GetType() == designCharacterAttributeType) ? (CCharacterAttributeType *)pType : NULL); }
-		virtual CCommunicationsHandler *GetCommsHandler (void) override { return m_Comms.GetCommsHandler(GetInheritFrom()); }
 		virtual CString GetNamePattern (DWORD dwNounFormFlags = 0, DWORD *retdwFlags = NULL) const;
-		virtual DesignTypes GetType (void) const override { return designGenericType; }
+		virtual DesignTypes GetType (void) const override { return designCharacterAttributeType; }
 
 	protected:
 		//	CDesignType overrides
@@ -26,6 +38,19 @@ class CCharacterAttributeType : public CDesignType
 		virtual void OnUnbindDesign (void) override;
 
 	private:
-		CCommunicationsStack m_Comms;
+
+		//	Character Attribute Category
+
+		ECharacterAttributeCategory m_iCategory = eCharacterCustomAttrib;
+		CString m_sCustomCategory = CONSTLIT("");
+
+		//	Language variants (ex, gendered variants)
+
+		TMap<CString, CLanguageDataBlock> m_LanguageVariants;
+
+		//	Art
+
+		CObjectImage m_ImgHero;				//	Hero image (e.g. character class or genome art)
+		CObjectImage m_ImgIcon;				//	Icon image (e.g. a small display icon for UIs) 
 
 	};
